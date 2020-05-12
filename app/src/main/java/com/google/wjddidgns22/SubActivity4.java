fb859bdf23f18d092b1a9615154c6df3ea63c012
donyoeb <43695307+donyoeb@users.noreply.github.com>
2020-05-12 19:33:55 +0900

    Update SubActivity4.java
    계획 변경

--- app/src/main/java/com/google/wjddidgns22/SubActivity4.java
+++ app/src/main/java/com/google/wjddidgns22/SubActivity4.java
@@ -1,166 +1,18 @@
 package com.google.wjddidgns22;
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 public class SubActivity4 extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-    private ArrayList<ItemObject> list = new ArrayList();
+   
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub4);
 
-        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
-
-        //AsyncTask 작동시킴(파싱)
-        new Description().execute();
+       
     }
 
-
-    private class Description extends AsyncTask<Void, Void, Void> {
-
-        //진행바표시
-        private ProgressDialog progressDialog;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-
-            //진행다일로그 시작
-            progressDialog = new ProgressDialog(SubActivity4.this);
-            progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-            progressDialog.setMessage("잠시 기다려 주세요.");
-            progressDialog.show();
-
-        }
-
-        @Override
-        protected Void doInBackground(Void... params) {
-            try {
-                Document doc = (Document) Jsoup.connect("https://movie.naver.com/movie/running/current.nhn").get();
-                Elements mElementDataSize = doc.select("ul[class=lst_detail_t1]").select("li");  //필요한 녀석만 꼬집어서 지정
-                int mElementSize = mElementDataSize.size(); //목록이 몇개인지 알아낸다. 그만큼 루프를 돌려야 하나깐.
-
-                for(org.jsoup.nodes.Element element : mElementDataSize){ //이렇게 요긴한 기능이
-                    //영화목록 <li> 에서 다시 원하는 데이터를 추출해 낸다.
-                    String my_title = element.select("li dt[class=tit] a").text();
-                    String my_link = element.select("li div[class=thumb] a").attr("href");
-                    String my_imgUrl = element.select("li div[class=thumb] a img").attr("src");
-                    //특정하기 힘들다... 저 앞에 있는집의 오른쪽으로 두번째집의 건너집이 바로 우리집이야 하는 식이다.
-                    Element rElem = element.select("dl[class=info_txt1] dt").next().first();
-                    String my_release = rElem.select("dd").text();
-                    Element dElem = element.select("dt[class=tit_t2]").next().first();
-                    String my_director = "감독: " + dElem.select("a").text();
-                    //Log.d("test", "test" + mTitle);
-                    //ArrayList에 계속 추가한다.
-                    list.add(new ItemObject(my_title, my_imgUrl, my_link, my_release, my_director));
-                }
-
-                //추출한 전체 <li> 출력해 보자.
-                Log.d("debug :", "List " + mElementDataSize);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            //ArraList를 인자로 해서 어답터와 연결한다.
-            MyAdapter myAdapter = new MyAdapter(list);
-            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
-            recyclerView.setLayoutManager(layoutManager);
-            recyclerView.setAdapter(myAdapter);
-
-            progressDialog.dismiss();
-        }
-    }
-}
-
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
-
-        //데이터 배열 선언
-        private ArrayList<ItemObject> mList;
-
-        public  class ViewHolder extends RecyclerView.ViewHolder {
-            private ImageView imageView_img;
-            private TextView textView_title, textView_release, texView_director;
-
-            public ViewHolder(View itemView) {
-                super(itemView);
-
-                imageView_img = (ImageView) itemView.findViewById(R.id.imageView_img);
-                textView_title = (TextView) itemView.findViewById(R.id.textView_title);
-                textView_release = (TextView) itemView.findViewById(R.id.textView_release);
-                texView_director = (TextView) itemView.findViewById(R.id.textView_director);
-            }
-        }
-
-        //생성자
-        public MyAdapter(ArrayList<ItemObject> list) {
-            this.mList = list;
-        }
-
-        @NonNull
-        @Override
-        public MyAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_sub4, parent, false);
-            return new ViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
-            holder.textView_title.setText(String.valueOf(mList.get(position).getTitle()));
-            holder.textView_release.setText(String.valueOf(mList.get(position).getRelease()));
-            holder.texView_director.setText(String.valueOf(mList.get(position).getDirector()));
-            //다 해줬는데도 GlideApp 에러가 나면 rebuild project를 해주자.
-            GlideApp.with(holder.itemView).load(mList.get(position).getImg_url())
-                    .override(300,400)
-                    .into(holder.imageView_img);
-        }
-
-    @Override
-    public int getItemCount() {
-        return mList.size();
-    }
 }
 
 
