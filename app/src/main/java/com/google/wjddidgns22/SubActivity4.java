286c4eb1e6fb44351c2d4868fdb1f3bf05d74793
leedonyoeb <duq2509@naver.com>
2020-05-19 21:01:10 +0900

    네이버 블로그 크롤링/ 한페이지 블로그 제목만 10개 추출하는데는 성공/ 리스트뷰에 제목들이 뜨지않는 문제있음

--- app/src/main/java/com/google/wjddidgns22/SubActivity4.java
+++ app/src/main/java/com/google/wjddidgns22/SubActivity4.java
@@ -1,12 +1,14 @@
 package com.google.wjddidgns22;
+
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import org.jsoup.Jsoup;
@@ -15,74 +17,174 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
 public class SubActivity4 extends AppCompatActivity {
 
-    public EditText editText; //결과를 띄어줄 TextView
-    public TextView textView; //결과를 띄어줄 TextView
-    public Button findbt; //reload버튼
+
+    public EditText editText; //검색할 edittext
+   // public TextView textView; //결과를 띄어줄 TextView
+    public Button findbt; //찾기 버튼
+    public Button matbt; //맛집
+
     public String topname;
-    Elements contents_name;
-    Elements contents_img;
+    public ListView list_view;
+
+    public String serch; //에딧텍스트에 입력한 단어
+    Elements contents; // 타이틀
+
     Document doc = null;
     String Top10;//결과를 저장할 문자열변수
 
+    private ArrayAdapter<String> adapter;
+    List<Object> Array = new ArrayList<Object>();
 
+    String k;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub4);
 
-
         Intent intent2 = getIntent();
         final String id = intent2.getExtras().getString("탑이름"); // 오벨리스크 이름
         topname = id;
 
-        Toast.makeText(SubActivity4.this, id ,Toast.LENGTH_SHORT).show();
-        editText = (EditText) findViewById(R.id.edtext);
-        textView = (TextView) findViewById(R.id.textBox);
-        findbt = (Button) findViewById(R.id.findbt);
+        Toast.makeText(SubActivity4.this, id ,Toast.LENGTH_SHORT).show(); // 이름 보여주기
 
-        findbt.setOnClickListener(new View.OnClickListener() {//onclicklistener를 연결하여 터치시 실행됨
+        editText = (EditText) findViewById(R.id.edtext);
+
+        list_view = (ListView)findViewById(R.id.lview);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, new ArrayList<String>());
+        list_view.setAdapter(adapter);
+
+
+
+        matbt = (Button) findViewById(R.id.matjip);
+        matbt.setOnClickListener(new View.OnClickListener() {//onclicklistener를 연결하여 터치시 실행됨
+
             @Override
             public void onClick(View v) {
+
+                Array.clear();
+                adapter.clear();
+
                 new AsyncTask() {//AsyncTask객체 생성
+                    @SuppressLint("WrongThread")
                     @Override
                     protected Object doInBackground(Object[] params) {
-                        String url;
-                        url = "https://search.naver.com/search.naver?where=post&sm=tab_jum&query=";  //네이버 블로그 url
-                        String serch = editText.getText().toString();  //에딧텍스트에 입력한 단어
 
-                        int cntli = 1; // 블로그 갯수
+                        String url,urlnum;
+                        //for (int i=1;i<102;i=i+10) { }
+                        url = "https://search.naver.com/search.naver?where=post&sm=tab_jum&query=";  //네이버 블로그 url
+                        serch = "맛집";
+                        //   urlnum = "&sm=tab_pge&srchby=all&st=sim&where=post&start=";
+                        //&sm=tab_pge&srchby=all&st=sim&where=post&start=1  // 1~10, 11~20. 21~30.....
+                        //  int urlcnt = 1;
+
+                        //  urlcnt= urlcnt+10;
                         try {
                             doc = Jsoup.connect(url+topname+serch).get(); //url + 탑이름 + 검색단어로 페이지를 불러옴
-                            contents_name = doc.select("div[class=blog section _blogBase _prs_blg]").
-                                    select("li[class=sh_blog_top]"); //필요한 녀석만 꼬집어서 지정
+                            contents = doc.select("div[class=blog section _blogBase _prs_blg]");
+                            int contentsize = contents.size();
+                            for (Element elem : contents){//li[id=sp_blog_1~10]
+                                for (int i = 1;i<11;i++){
+                                    String title = elem.select("li[id=sp_blog_"+i+"]").select("a[title]").text();
+                                    Array.add(title);
+                                }
+                            }
 
 
-                            cntli++;
-                        } catch (IOException e) {
+                        }
+                        catch (IOException e) {
                             e.printStackTrace();
                         }
-                        Top10 = "";
-                        int cnt = 0;//숫자를 세기위한 변수
-                        for(Element element: contents_name) {
-                            cnt++;
-                            Top10 += cnt+". "+element.text() + "\n";
-                            if(cnt == 10)//10위까지 파싱하므로
-                                break;
-                        }
+
+
+
                         return null;
                     }
                     @Override
                     protected void onPostExecute(Object o) {
                         super.onPostExecute(o);
-                        Log.i("TAG",""+Top10);
-                        textView.setText(Top10);
+
+                        adapter.notifyDataSetChanged(); // 어댑터리스트 갱신
+                        list_view.setSelection(adapter.getCount() - 1);
+
+                        Toast.makeText(SubActivity4.this,"검색결과: "+Array.size(),Toast.LENGTH_LONG).show();
                     }
                 }.execute();
+
+
             }
+
         });
+
+
+/*
+        findbt = (Button) findViewById(R.id.findbt); //검색버튼
+        findbt.setOnClickListener(new View.OnClickListener() {//onclicklistener를 연결하여 터치시 실행됨
+
+            @Override
+            public void onClick(View v) {
+                adapter.clear();
+                Array.clear();
+                new AsyncTask() {//AsyncTask객체 생성
+                    @SuppressLint("WrongThread")
+                    @Override
+                    protected Object doInBackground(Object[] params) {
+                        String url,urlnum;
+                        url = "https://search.naver.com/search.naver?where=post&sm=tab_jum&query=";  //네이버 블로그 url
+
+                        serch = editText.getText().toString();
+                     //   urlnum = "&sm=tab_pge&srchby=all&st=sim&where=post&start=";
+                        //&sm=tab_pge&srchby=all&st=sim&where=post&start=1  // 1~10, 11~20. 21~30.....
+                      //  int urlcnt = 1;
+
+                        int cnt = 0;//숫자를 세기위한 변수
+                        try {
+                            doc = Jsoup.connect(url+topname+serch).get(); //url + 탑이름 + 검색단어로 페이지를 불러옴
+                            contents= doc.select("div[class=blog section _blogBase _prs_blg]")
+                                    .select("li[class=sh_blog_top]").select("id.sp_blog_1").select("a[title]"); //필요한 녀석만 꼬집어서 지정
+
+
+
+                          //  urlcnt= urlcnt+10;
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+
+                        for(Element element: contents) {
+                            cnt++;
+                            k = element.text();
+                        //      Array.add(element.text()); // array에 저장
+                        //    adapter.add(element.text()); //adapter에 저장
+
+                        if(cnt == 10)//10위까지 파싱하므로
+                            break;
+                    }
+                        return null;
+                    }
+                    @Override
+                    protected void onPostExecute(Object o) {
+                        super.onPostExecute(o);
+
+                     //   adapter.notifyDataSetChanged(); // 어댑터리스트 갱신
+                  //      list_view.setSelection(adapter.getCount() - 1);
+                       // textView.setText(Top10);
+
+                        Toast.makeText(SubActivity4.this,"검색결과: "+ Array.size()+k,Toast.LENGTH_LONG).show();
+                    }
+                }.execute();
+
+
+            }
+        });*/
     }
+
+
 }
